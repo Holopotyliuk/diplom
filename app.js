e209d9dd4fbcd133426be15b8184c399c36c5f15
Holopotyliuk <golopotiluka@gmail.com>
2023-11-09 16:36:24 +0200

    implemented sending a response to a get request

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.use(express.json())
+app.use('/info', (req, res) => {
+    res.json("Hello it`s server");
+  });
 
 module.exports=app
 
