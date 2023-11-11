4d919cc9c1521180a876eed67c4d99d13ab23f85
Holopotyliuk <golopotiluka@gmail.com>
2023-11-11 16:16:06 +0200

    finalized the registration and checked the correctness of the entered data

--- server/models/models.js
+++ server/models/models.js
@@ -1,20 +1,22 @@
+const { check } = require('express-validator');
 const pool = require('../connection/connection')
 const bcrypt = require('bcryptjs');
 async function registration(req, res) {
     try {
-        const { login, password } = req.body;
-        const hashPassword = bcrypt.hashSync(password, 7);
-        console.log(login)
-        const userCheck = await pool.query('select * from users where login=$1', [login])
-        console.log(userCheck.rows)
-        if (userCheck.rows.length == 0) {
-            const user = await pool.query('insert into users (login, password) values ($1, $2) returning *',
-                [login, hashPassword]);
-            res.json(user.rows)
+        const check = checkRequest(req);
+        if (check.status) {
+            const { name, login, password } = req.body;
+            const hashPassword = bcrypt.hashSync(password, 7);
+            const userCheck = await pool.query('select * from users where login=$1', [login])
+            if (userCheck.rows.length == 0) {
+                const user = await pool.query('insert into users (name, login, password) values ($1, $2, $3) returning *',
+                    [name, login, hashPassword]);
+                res.json(user.rows)
+            } else {
+                res.status(400).json({ message: `Користувач вже існує` })
+            }
         } else {
-            res.status(400).json({ message: `Користувач вже існує` })
-
-            // res.json('user.rows')
+            console.log(check.message)
         }
     } catch (error) {
         console.log(error)
@@ -28,7 +30,7 @@ async function authorization(req, res) {
         if (userCheck.rows.length !== 0) {
             const validPassword = bcrypt.compareSync(password, userCheck.rows[0].password);
             if (validPassword) {
-
+                res.json('true')
             } else {
                 res.status(400).json({ message: `Не правильний пароль` })
             }
@@ -39,5 +41,39 @@ async function authorization(req, res) {
         console.log(error)
     }
 
+}
+function checkRequest(req) {
+    const { name, login, password } = req.body;
+    const check = {
+        status: 0,
+        message: ''
+    };
+    
+    if (name) {
+        check.status = true
+    } else {
+        check.status = false
+        check.message = 'Введіть ім`я';
+        return check;
+    }
+    const cleanedLogin = login.replace(/\s/g, '');
+    const isValid = /^\d{12}$/.test(cleanedLogin) && cleanedLogin.startsWith('380');
+    if (isValid) {
+        check.status = true
+    } else {
+        check.status = false
+        check.message = 'Не коректний номер';
+        return check;
+    }
+    const digitCount = password.toString().replace(/\D/g, '').length;
+    const lenghtPassword = digitCount >= 8 && digitCount <= 20;
+    if (lenghtPassword) {
+        check.status = true
+    } else {
+        check.status = false
+        check.message = 'Не коректний пароль';
+        return check;
+    }
+    return check;
 }
 module.exports = { registration, authorization }
